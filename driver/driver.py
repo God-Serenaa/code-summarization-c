f76b93d697903f9a8f2b1d62c34b7cc6274e450c
IshmamR <ishmam785@gmail.com>
2023-12-29 20:58:05 +0600

    beginning of an end

--- driver/driver.py
+++ driver/driver.py
@@ -1,5 +1,3 @@
-from utils.utils import preprocess_code, detect_functions, extract_function, count_function_calls
-
 def driver(PATH):
   with open(PATH) as f:
     code_lines = f.readlines()
@@ -8,13 +6,4 @@ def driver(PATH):
     for line in code_lines:
       code += "\n"+line
 
-    code_string = preprocess_code(code)
-    functions = detect_functions(code_string)
-    
-    function_call = []
-    for function in functions:
-      temp = {"name": function}
-      temp["call_count"] = count_function_calls(code, function)
-      function_call.append(temp)
-    
-    return function_call
+    return code
