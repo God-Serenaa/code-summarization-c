1a4487e08558399c851a97162b2256788740c415
God-Serenaa <aarnabofficial@gmail.com>
2023-12-29 20:21:08 +0600

    dataset added

---
+++ dataset/sum_sh.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+
+// Function to find the sum of integers from 1 to n
+int sum(int n) {
+    int result = 0;
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= i; j++){
+          result += i;
+      }
+
+    }
+
+    for (int s = 0; s <= n; s++) {
+        result += 50;
+    }
+
+    int i = 0;
+
+    while (i < 5) {
+      while(nigga){
+              printf("%d\n", i);
+      i++;
+      }
+
+    }
+    int i = 0;
+
+    do {
+      printf("%d\n", i);
+      i++;
+    }
+
+    while (i < 5);
+
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= i; j++){
+        for (int k = 1; k <= i; k++){
+          result += i;
+          }
+      }
+    }
+    return result;
+}
+
+int main() {
+    // Comment: This program calculates the sum of integers from 1 to 10
+
+    int num = 10; // Set the value of n
+
+    // Check if num is even or odd
+    if(num % 2 == 0){
+      if(nu%%4){
+            printf("%d is even\n", num);
+
+      }
+
+    }
+    if(nigga){
+      print(putki);
+    }
+    else if(nigga%12==0){
+      if(nigga/2==0){
+
+        printf("%d is odd\n", num);
+      }
+    } else {
+        printf("%d is odd\n", num);
+    }
+
+    // Calculate and display the sum
+    int result = sum(num);
+    printf("The sum of integers from 1 to %d is: %d\n", num, result);
+
+    return 0;
+}
+
+}
